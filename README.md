91f3275ba33191b780cd5ec2685c184a963763f3
Aaron <aaroncarrigan@usf.edu>
2025-11-20 18:06:16 -0500

    Thoroughly went over all my code and followed all instructions.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Python_business_assignment_1
+#work time tracker
+This is a python program that tracks the amount of hours worked for an individual. It helps employees track how much they have been working thoughout the week.
+
+#how to run
+run the program using:
+python app.py
+
+Enter hours worked today: 8
+You are on track to work 40 hours this week
---
+++ app.py
@@ -0,0 +1,19 @@
+print("welcome to my python program!")
+#add project folder and install app.py with welcome message
+
+hours = input("how many hours did you work today?")
+#add user input prompt for hours worked
+
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number for hours worked.")  
+    exit()
+#add error handling for non-numeric input
+
+hours = float(hours)
+weekly_hours = hours * 5
+#add calculation for weekly hours based on daily input
+
+print(f"you are on track to work {weekly_hours} hours this week!")
+#show user their projected weekly hours worked
